6e13c48cf8afa46e7cd6881bb0921251b5932e3f
FioreriaAlessandrino <43786232+FioreriaAlessandrino@users.noreply.github.com>
2024-06-25 15:31:27 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { ChakraProvider, ColorModeScript, extendTheme, VStack } from "@chakra-ui/react";
+import "filepond/dist/filepond.min.css";
+import { useAsync, useObservable } from "react-use";
+
+import { ViewController, ViewControllerProvider } from "./ViewController";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { LoaderScreen } from "./components/LoaderScreen";
+import { Members } from "./components/Members";
+import { Navbar } from "./components/Navbar";
+import AuthService from "./services/auth";
+import ErrorHandlingService from "./services/error-handling";
+import FirebaseService from "./services/firebase";
+import "./util/base.css";
+import theme from "./util/theme";
+import React from "react";
+import RedirectPage from './components/RedirectPage';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+
+
+function App() {
+  const setupState = useAsync(async () => {
+    try {
+      await FirebaseService.I.init();
+    } catch (e) {
+      ErrorHandlingService.I.notifyUserOfError("Error while initializing Firebase", e);
+      return;
+    }
+    try {
+      await AuthService.I.authenticate();
+    } catch (e) {
+      ErrorHandlingService.I.notifyUserOfError("Error during anonymous authorization", e);
+      return;
+    }
+  });
+
+  const userId = useObservable(AuthService.I.userId$);
+
+  return (
+    <ViewControllerProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <ChakraProvider resetCSS theme={extendTheme(theme)}>
+        {(setupState.loading || setupState.error) ? (
+          <>
+            <LoaderScreen />
+            <Members />
+          </>
+        ) : (
+          <VStack justify="stretch" h="full" w="full">
+            <Router>
+              <Routes>
+                <Route path="/UrlShortener/:id" element={<RedirectPage />} />
+              </Routes>
+              <Navbar />
+              <Hero />
+              <Footer />
+            </Router>
+          </VStack>
+        )}
+        {!!userId && <ViewController />}
+      </ChakraProvider>
+    </ViewControllerProvider>
+  );
+}
+
+export default App;
